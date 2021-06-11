fd402e4a2aae23e9bfb6a551bdc13771231ee2b4
Tim Randall <trtimrandall@gmail.com>
2021-06-11 19:48:25 +1000

    Initial Commit

---
+++ HangmanSwift/HangmanSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanSwiftApp.swift
+//  HangmanSwift
+//
+//  Created by Tim Randall on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HangmanSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
